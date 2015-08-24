7d3c7b2da2858dc0d4f01a2b2567c0f6dca82c05
Alex Niderberg <aniderberg@gmail.com>
2015-08-24 18:02:27 -0400

    Initial Commit

---
+++ wineEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wineEngine
+//
+//  Created by Niderberg, Alex on 8/24/15.
+//  Copyright (c) 2015 Niderberg, Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
