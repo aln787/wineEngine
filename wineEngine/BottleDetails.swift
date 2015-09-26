4702c438c1ed535bd23295a706939d9a32f8e230
Alex Niderberg <aniderberg@gmail.com>
2015-09-26 08:16:43 -0400

    Beginning to add up dated UI with Danny.

---
+++ wineEngine/BottleDetails.swift
@@ -0,0 +1,11 @@
+//
+//  BottleDetails.swift
+//  wineEngine
+//
+//  Created by Niderberg, Alex on 9/24/15.
+//  Copyright (c) 2015 Niderberg, Alex. All rights reserved.
+//
+
+import Foundation
+
+demensions = ["Body", "Sweetness", "Tannin", "Acidity", "Color Depth"]
